9f4db7432bf26664d8529e50e13c9e01269b68a6
Knyazev Nikita <nk8222138@gmail.com>
2024-05-24 01:15:32 +0300

    first commit

---
+++ Task_2/Task_2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <set>
+#include <string>
+
+struct compare
+{
+	bool operator()(const int& a, const int& b) const
+	{
+		return a > b;
+	}
+};
+
+int main()
+{
+	std::string count_numbers;
+	std::set<int, compare> numbers;
+	int num{};
+
+	std::cout << "Count numbers: ";
+	getline(std::cin, count_numbers);
+	std::cout << std::endl;
+
+	int i = stoi(count_numbers); 
+	for (i; i > 0; --i) {
+		std::cin >> num;
+		numbers.insert(num);
+	}
+
+	for (const auto& c : numbers)
+		std::cout << std::endl << c ;
+
+	return 0;
+}
